2c88e413886bea3a454f6246268c8407d6840a8c
Gregory Hogg <gahogg@uwaterloo.ca>
2020-06-28 14:14:07 -0400

    Adding images and buttons

--- MyApp/MainPage.xaml.cs
+++ MyApp/MainPage.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace MyApp
 {
@@ -16,6 +18,25 @@ namespace MyApp
         public MainPage()
         {
             InitializeComponent();
+        }
+    }
+
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(System.IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+
+            return imageSource;
         }
     }
 }
