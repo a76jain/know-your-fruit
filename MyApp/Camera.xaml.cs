42b43026a17acaa7375494f7575da78f2a3afe55
Gregory Hogg <gahogg@uwaterloo.ca>
2020-07-25 21:57:35 -0400

    Add photo from client functionality

--- MyApp/Camera.xaml.cs
+++ MyApp/Camera.xaml.cs
@@ -2,6 +2,11 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Plugin.Media;
+using Flurl.Http;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace MyApp
 {
@@ -19,6 +24,34 @@ namespace MyApp
 
             if (photo != null)
                 PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+
+            byte[] photoByteArr;
+
+            // Convert photo to array of bytes
+            using (var memoryStream = new MemoryStream())
+            {
+                photo.GetStream().CopyTo(memoryStream);
+                photo.Dispose();
+                photoByteArr = memoryStream.ToArray();
+            }
+
+            Task<HttpResponseMessage> response = UploadAsFormDataContent("https://kyf-cloud-api.herokuapp.com/", photoByteArr);
+            HttpResponseMessage httpResponse = response.Result;
+            String responseBodyAsText = await httpResponse.Content.ReadAsStringAsync();
+            responseBodyAsText = responseBodyAsText.Replace("<br>", Environment.NewLine); // todo remove
+            Console.WriteLine(responseBodyAsText);
         }
+
+        public Task<HttpResponseMessage> UploadAsFormDataContent(string url, byte[] image)
+        {
+            MultipartFormDataContent form = new MultipartFormDataContent
+            {
+                { new ByteArrayContent(image, 0, image.Length), "image", "pic.jpeg" }
+            };
+
+            HttpClient client = new HttpClient();
+            return client.PostAsync(url, form);
+        }
+
     }
 }
